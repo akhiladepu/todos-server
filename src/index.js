caa23d89f18f04baa1d038a91f905c7a43c6a31e
akhiladepu <akhil9394021176@gmail.com>
2022-03-25 20:33:05 +0530

    env updated

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ const cors = require("cors")
 const app = express();
 app.use(cors());
 app.use(express.json());
+require("dotenv").config();
 const connect = require("./configs/db");
+
+const PORT = process.env.PORT;
+
 const todoController = require("./controllers/todo.controller");
 
 app.use("/", todoController);
 
-app.listen(2345, async () => {
+app.listen(PORT, async () => {
     try {
         await connect();
         console.log("Connected to db at 2345");
